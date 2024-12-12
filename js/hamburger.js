4f8c51e3c932cfcf3341220c642f9b8ea1c6bff7
andylikesyarn <stowersandy3@gmail.com>
2024-12-12 12:22:16 -0500

    updating hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,12 +1,13 @@
 const burgerButton = document.getElementById("burger-button");
 const menuCloseIcon = document.getElementById("menu-closed");
 const menuOpenIcon = document.getElementById("menu-open");
-const navMenu = document.getElementById("navnavnav");
+const navMenu = document.getElementById("navbar");
 
 function toggleMenu() {
   menuCloseIcon.classList.toggle("visible");
   menuOpenIcon.classList.toggle("visible");
   navMenu.classList.toggle("visible");
+  console.log("button clicked");
 }
 
 burgerButton.addEventListener("click", toggleMenu);
